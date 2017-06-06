6b2cad3b5cf796168bef863008868fb1615a8616
Jason Vandewarker <jasonvandewarker@gmail.com>
2017-06-06 10:17:24 -0700

    Tuesday, some of the front end changed.

--- src/app/app.js
+++ src/app/app.js
@@ -2,74 +2,61 @@
     'use strict';
 
     angular
-        .module('app', ['oitozero.ngSweetAlert', 'socialLogin', 'ui.router'])
-        .config(function($stateProvider, $urlRouterProvider, ) {
+        .module('app', ['oitozero.ngSweetAlert', 'socialLogin', 'ui.router', 'socialLogin', 'ui.bootstrap'])
+        .value('localApi', 'http://localhost:59820/api/')
+        .config(function($stateProvider, $urlRouterProvider, socialProvider) {
+
+            $urlRouterProvider.otherwise("/main");
 
             $stateProvider
-                .state('index', {
-                    url: "/index",
-                    templateUrl: "index.html",
+                .state('main', {
+                    url: "/main",
+                    templateUrl: "main.html",
                     controller: "HomeController",
                     controllerAs: "HomeCtrl"
                 });
 
             $stateProvider
                 .state('login', {
-                    url: "/app/home/fblogin.html",
+                    url: "/fblogin",
                     templateUrl: "app/home/fblogin.html",
                     controller: "HomeController",
                     controllerAs: "HomeCtrl"
-
                 });
 
-        });
+            $stateProvider
+                .state('productfeed', {
+                    url: "/productfeed",
+                    templateUrl: "/app/products/product.feed.html",
+                    controller: "ProductController",
+                    controllerAs: "ProductCtrl"
+                });
+
+
+
+            // $stateProvider
+            //     .state('message', {
+            //         url: "/app/message/message.html",
+            //         templateUrl: "app/message/message.html",
+            //         controller: "MessageController",
+            //         controllerAs: "MessageCtrl"
+            //     });
+
+            // $stateProvider
+            //     .state('favorites', {
+            //         url: "/app/user/favorites.html",
+            //         templateUrl: "app/user/favorites.html",
+            //         controller: "UserController",
+            //         controllerAs: "UserCtrl"
+            //     });
+
+
+            //socialProvider.setFbKey
+
+
+
+
+
+        }); // end of angular module
 
 })();
-
-
-
-
-
-// (function() {
-//     'use strict';
-
-//     var app = angular.module('app', ['ui.router', 'LocalStorageModule']).value('localApi', 'http://localhost:59820/api/');
-
-//     app.config(function($stateProvider, $urlRouterProvider, localStorageServiceProvider) {
-
-//         localStorageServiceProvider.setPrefix('app').setStorageType('sessionStorage').setNotify(true, true);
-//         // For any unmatched url, redirect to /main
-//         $urlRouterProvider.otherwise("/index");
-
-//         $stateProvider
-//             .state('index', {
-//                 url: "/",
-//                 templateUrl: "index.html",
-//                 controller: "HomeController",
-//                 controllerAs: "HomeCtrl"
-//             })
-
-//         .state("product", {
-//             url: "/products",
-//             templateUrl: "products/product.category.html",
-//             controller: "ProductController",
-//             controllerAs: "ProductCtrl"
-//         })
-
-//         .state("user", {
-//             url: "/profile",
-//             templateUrl: "user/profile.html",
-//             controller: "UserController",
-//             controllerAs: "UserCtrl"
-//         })
-
-//         .state("message", {
-//             url: "/message",
-//             templateUrl: "message/message.html",
-//             controller: "MessageController",
-//             controllerAs: "MessageCtrl"
-//         })
-
-//     });
-
-// })();
--- src/app/home/home.controller.js
+++ src/app/home/home.controller.js
@@ -5,13 +5,35 @@
         .module('app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['SweetAlert', '$state'];
+    HomeController.$inject = ['HomeFactory', 'SweetAlert', '$state'];
 
-    function HomeController(SweetAlert, $state) {
+    function HomeController(HomeFactory, SweetAlert, $state) {
         var vm = this;
         //vm.loginButton = loginButton;
+        vm.links = [{
+            'display': 'Welcome User!',
+            'state': 'login'
+        }, {
+            'display': 'Product Feed',
+            'state': 'productfeed'
+        }, ];
 
-        ////////////////
+        vm.status = {
+            isopen: false
+        };
+
+        ///////////////////////////////////////////////////////
+
+        vm.toggled = function(open) {
+            $log.log('Dropdown is now: ', open);
+        };
+
+        vm.toggleDropdown = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            vm.status.isopen = !vm.status.isopen;
+        };
+
 
         //     function loginButton() {
         //         swal({
@@ -26,13 +48,6 @@
         //         });
         //     };
         // }
-
-
-
-
-
-
-
 
 
     };
