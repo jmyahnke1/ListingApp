0a5ace780f9178c1c74fa215bd5ad81fd8049728
Jason Vandewarker <jasonvandewarker@gmail.com>
2017-06-13 13:03:53 -0700

    Tuesday afternoon.

--- src/app/message/message.factory.js
+++ src/app/message/message.factory.js
@@ -1,34 +1,34 @@
-// (function() {
-//     'use strict';
+(function() {
+    'use strict';
 
-//     angular
-//         .module('app')
-//         .factory('MessageFactory', MessageFactory);
-//     MessageFactory.$inject = ['$http', 'localApi'];
+    angular
+        .module('app')
+        .factory('MessageFactory', MessageFactory);
+    MessageFactory.$inject = ['$http', 'localApi'];
 
-//     /* @ngInject */
-//     function MessageFactory($http, localApi) {
-//         var service = {
-//             getMessage: getMessage,
-//             postMessage: postMessage
+    /* @ngInject */
+    function MessageFactory($http, localApi) {
+        var service = {
+            getMessage: getMessage,
+            postMessage: postMessage
 
-//         };
+        };
 
-//         return service;
+        return service;
 
-//         function getMessageById(id) {
-//             return $http({
-//                 Method: 'GET',
-//                 url: 'http://localhost:59820/api' + 'Messages' + UserId,
-//                 params: searchParameters
-//             }).then(function(response) {
-//                 return response.data.messages[0];
-//             }, function(error) {
-//                 console.log("Error" + error);
-//                 return error;
-//             });
-//         } //end of getMessage function
+        function getMessageById(id) {
+            return $http({
+                Method: 'GET',
+                url: 'http://localhost:59820/api' + 'Messages' + UserId,
+                params: searchParameters
+            }).then(function(response) {
+                return response.data.messages[0];
+            }, function(error) {
+                console.log("Error" + error);
+                return error;
+            });
+        } //end of getMessage function
 
 
-//     }
-// })();
+    }
+})();
